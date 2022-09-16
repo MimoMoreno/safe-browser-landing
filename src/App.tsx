7a9b4562ce06fe7a8013b659798ef21354006e29
MimoMoreno <mmorenolafon@gmail.com>
2022-09-16 20:16:00 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="safe-browser-title">
+          <h1>SafeBrowser</h1>
+          <h1>SafeBrowser</h1>
+        </div>
+        {/* <div className="title">
+          Start surfing freely and safely <b>now!</b>
+        </div> */}
+        <div className="description">
+          Download the Safe Browser app and start surfing around all the Internet through ipfs
+        </div>
+        <button>DOWNLOAD</button>
       </header>
     </div>
   );
