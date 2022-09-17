cd5a5f13b65f78dfe10be749a3377bb20729f692
Diego Vazquez <diego.vazquez@valoraapp.com>
2022-09-17 17:07:00 -0300

    Adding download links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 
+const debianDownloadLink = 'https://drive.google.com/uc?export=download&id=1WNh2CAPPHDKt6LvE0yEoPKWNbxTiuLMz'
+const macDownloadLink = 'https://drive.google.com/uc?export=download&id=1iidztMakMifRE_dxuIkOq33SxGMr7HUv'
+
 function App() {
   return (
     <div className="App">
@@ -12,7 +15,11 @@ function App() {
         <div className="description">
           Navigate the web without depending on any centralized providers.
         </div>
-        <button>DOWNLOAD</button>
+        <div style={{display: 'flex', gap: '32px', marginTop: '80px'}}>
+          <a href={debianDownloadLink} download><button style={{cursor: 'pointer'}}> Download for Debian x64</button></a>
+          <a href={macDownloadLink} download><button style={{cursor: 'pointer'}}>Download for MacOS x64</button></a>
+        </div>
+
         <div className="instructions">
           or{" "}
           <a
